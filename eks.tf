065df425cc3ab881c9eaa47c18df936548dfe443
Praveen-1905 <pk8583143@gmail.com>
2026-03-04 17:32:01 +0530

    Add EKS module configuration in eks.tf

---
+++ eks.tf
@@ -0,0 +1,21 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "21.15.1"
+
+  name    = "demo-cluster"
+  kubernetes_version = "1.29"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  endpoint_public_access = true
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.medium"]
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 2
+    }
+  }
+}
